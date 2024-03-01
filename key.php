f338b34c626cc24c5a9182b4dd48fe7713ca7c44
KALAR-RIYA <147729940+KALAR-RIYA@users.noreply.github.com>
2024-03-01 20:18:36 +0530

    hospital appointment website
    my first project

---
+++ key.php
@@ -0,0 +1,11 @@
+<?php
+ $color =
+ ["red"=>1,
+ "black"=>2,
+ "green=>"3,
+ "blue"=>4];
+ array_keys($color);
+ array_key_first($color);
+ array_key_last($color);
+ array_key_exists(3,$color);
+?>
---
+++ nested array.php
@@ -0,0 +1,13 @@
+<?php
+$emp=[
+	[1,"riya","rai",4000],
+	[2,"reshu","owner",3000],
+	[3,"rashi","computer owner",7000],
+	[1,"reena","employee",60000],
+	[1,"radha","operator",4000]];
+	echo"<pre>";
+	print_r($emp);
+	echo"</pre>";
+
+
+?>
